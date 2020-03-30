5d7c93d5771ebfac633594eb0f1c093e9cdd1e21
Paolo Vega <pao02esc@gmail.com>
2020-03-29 23:37:08 -0600

    Part 2 changes and code for Google Maps API

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -2,4 +2,5 @@
 weather_api_key = "48358c422f8f1cc9d5406d6f72c5cb4c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+#g_key = "AIzaSyAWSEiTSSsTkU7S0FowR7Rde61Cf98llJ4"
+g_key = "AIzaSyDI1BnuSmCpruEHuHGK7UiOqPrrHVvu3c8"
