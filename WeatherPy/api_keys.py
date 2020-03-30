290d4fa4f50f3f0a53771c82ef54c48ad15ffa31
Paolo Vega <pao02esc@gmail.com>
2020-03-29 23:42:42 -0600

    Updates to gitignore

--- WeatherPy/api_keys.py
+++
@@ -1,6 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "48358c422f8f1cc9d5406d6f72c5cb4c"
-
-# Google API Key
-#g_key = "AIzaSyAWSEiTSSsTkU7S0FowR7Rde61Cf98llJ4"
-g_key = "AIzaSyDI1BnuSmCpruEHuHGK7UiOqPrrHVvu3c8"
